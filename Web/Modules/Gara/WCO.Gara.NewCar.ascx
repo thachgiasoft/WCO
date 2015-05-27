e0f1417028d0cd6f6394f4157405bb1caef038e4
huamanhtuyen <huamanhtuyen@gmail.com>
2015-05-27 12:14:20 +0700

    Thay thế control TextBox

--- Web/Modules/Gara/WCO.Gara.NewCar.ascx
+++ Web/Modules/Gara/WCO.Gara.NewCar.ascx
@@ -12,78 +12,78 @@
                     <tr>
                         <td style="width: 17%">Biển số</td>
                         <td style="width: 15%">
-                            <cc1:mojoTextArea ID="txtBienSo" runat="server" Width="90%"></cc1:mojoTextArea></td>
+                            <asp:TextBox ID="txtBienSo" runat="server" Width="90%"></asp:TextBox></td>
                         <td colspan="4"></td>
                     </tr>
                     <tr>
                         <td>Mã kiểu xe</td>
                         <td>
-                            <cc1:mojoTextArea ID="MojoTextArea7" runat="server" Width="90%"></cc1:mojoTextArea></td>
+                            <asp:TextBox ID="MojoTextArea7" runat="server" Width="90%"></asp:TextBox></td>
                         <td colspan="4">
-                            <cc1:mojoTextArea ID="MojoTextArea8" runat="server" Width="90%"></cc1:mojoTextArea></td>
+                            <asp:TextBox ID="MojoTextArea8" runat="server" Width="90%"></asp:TextBox></td>
                     </tr>
                     <tr>
                         <td>Năm sản xuất</td>
                         <td>
-                            <cc1:mojoTextArea ID="txtNamSX" runat="server" Width="90%"></cc1:mojoTextArea></td>
+                            <asp:TextBox ID="txtNamSX" runat="server" Width="90%"></asp:TextBox></td>
                         <td style="width: 15%">Số khung</td>
                         <td style="width: 19%">
-                            <cc1:mojoTextArea ID="MojoTextArea1" runat="server" Width="90%"></cc1:mojoTextArea></td>
+                            <asp:TextBox ID="MojoTextArea1" runat="server" Width="90%"></asp:TextBox></td>
                         <td style="width: 15%">Số máy</td>
                         <td style="width: 19%">
-                            <cc1:mojoTextArea ID="MojoTextArea4" runat="server" Width="90%"></cc1:mojoTextArea></td>
+                            <asp:TextBox ID="MojoTextArea4" runat="server" Width="90%"></asp:TextBox></td>
                     </tr>
                     <tr>
                         <td>Ngày mua xe</td>
                         <td>
-                            <cc1:mojoTextArea ID="MojoTextArea2" runat="server" Width="90%"></cc1:mojoTextArea></td>
+                            <asp:TextBox ID="MojoTextArea2" runat="server" Width="90%"></asp:TextBox></td>
                         <td style="width: 15%">Màu xe</td>
                         <td style="width: 19%">
-                            <cc1:mojoTextArea ID="MojoTextArea5" runat="server" Width="90%"></cc1:mojoTextArea></td>
+                            <asp:TextBox ID="MojoTextArea5" runat="server" Width="90%"></asp:TextBox></td>
                         <td style="width: 15%">Mã nội thất</td>
                         <td style="width: 19%">
-                            <cc1:mojoTextArea ID="MojoTextArea6" runat="server" Width="90%"></cc1:mojoTextArea></td>
+                            <asp:TextBox ID="MojoTextArea6" runat="server" Width="90%"></asp:TextBox></td>
                     </tr>
                     <tr>
                         <td>Mã khách</td>
                         <td>
-                            <cc1:mojoTextArea ID="MojoTextArea3" runat="server" Width="90%"></cc1:mojoTextArea></td>
+                            <asp:TextBox ID="MojoTextArea3" runat="server" Width="90%"></asp:TextBox></td>
                         <td colspan="4"></td>
                     </tr>
                     <tr>
                         <td>Tên khách</td>
                         <td colspan="4">
-                            <cc1:mojoTextArea ID="MojoTextArea9" runat="server" Width="90%"></cc1:mojoTextArea></td>
+                            <asp:TextBox ID="MojoTextArea9" runat="server" Width="90%"></asp:TextBox></td>
                         <td></td>
                     </tr>
                     <tr>
                         <td>Điện thoại </td>
                         <td colspan="2">
-                            <cc1:mojoTextArea ID="MojoTextArea10" runat="server" Width="90%"></cc1:mojoTextArea></td>
+                            <asp:TextBox ID="MojoTextArea10" runat="server" Width="90%"></asp:TextBox></td>
                         <td></td>
                     </tr>
                     <tr>
                         <td>Địa chỉ</td>
                         <td colspan="4">
-                            <cc1:mojoTextArea ID="MojoTextArea14" runat="server" Width="90%"></cc1:mojoTextArea></td>
+                            <asp:TextBox ID="MojoTextArea14" runat="server" Width="90%"></asp:TextBox></td>
                         <td></td>
                     </tr>
                     <tr>
                         <td>Lái xe </td>
                         <td colspan="2">
-                            <cc1:mojoTextArea ID="MojoTextArea11" runat="server" Width="90%"></cc1:mojoTextArea></td>
+                            <asp:TextBox ID="MojoTextArea11" runat="server" Width="90%"></asp:TextBox></td>
                         <td></td>
                     </tr>
                     <tr>
                         <td>Điện thoại </td>
                         <td colspan="2">
-                            <cc1:mojoTextArea ID="MojoTextArea12" runat="server" Width="90%"></cc1:mojoTextArea></td>
+                            <asp:TextBox ID="MojoTextArea12" runat="server" Width="90%"></asp:TextBox></td>
                         <td></td>
                     </tr>
                     <tr>
                         <td>Mã số thuế </td>
                         <td colspan="2">
-                            <cc1:mojoTextArea ID="MojoTextArea13" runat="server" Width="90%"></cc1:mojoTextArea></td>
+                            <asp:TextBox ID="MojoTextArea13" runat="server" Width="90%"></asp:TextBox></td>
                         <td></td>
                     </tr>
                     <tr style="text-align: center">
--- Web/Modules/Gara/WCO.Gara.NewCar.ascx.designer.cs
+++ Web/Modules/Gara/WCO.Gara.NewCar.ascx.designer.cs
@@ -11,5 +11,212 @@ namespace WCO.Gara {
     
     
     public partial class NewCar {
+        
+        /// <summary>
+        /// pnlOuterWrap control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::mojoPortal.Web.UI.OuterWrapperPanel pnlOuterWrap;
+        
+        /// <summary>
+        /// ctop1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::mojoPortal.Web.Controls.CornerRounderTop ctop1;
+        
+        /// <summary>
+        /// pnlInnerWrap control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::mojoPortal.Web.UI.InnerWrapperPanel pnlInnerWrap;
+        
+        /// <summary>
+        /// upNewCar control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel upNewCar;
+        
+        /// <summary>
+        /// txtBienSo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtBienSo;
+        
+        /// <summary>
+        /// MojoTextArea7 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox MojoTextArea7;
+        
+        /// <summary>
+        /// MojoTextArea8 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox MojoTextArea8;
+        
+        /// <summary>
+        /// txtNamSX control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNamSX;
+        
+        /// <summary>
+        /// MojoTextArea1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox MojoTextArea1;
+        
+        /// <summary>
+        /// MojoTextArea4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox MojoTextArea4;
+        
+        /// <summary>
+        /// MojoTextArea2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox MojoTextArea2;
+        
+        /// <summary>
+        /// MojoTextArea5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox MojoTextArea5;
+        
+        /// <summary>
+        /// MojoTextArea6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox MojoTextArea6;
+        
+        /// <summary>
+        /// MojoTextArea3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox MojoTextArea3;
+        
+        /// <summary>
+        /// MojoTextArea9 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox MojoTextArea9;
+        
+        /// <summary>
+        /// MojoTextArea10 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox MojoTextArea10;
+        
+        /// <summary>
+        /// MojoTextArea14 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox MojoTextArea14;
+        
+        /// <summary>
+        /// MojoTextArea11 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox MojoTextArea11;
+        
+        /// <summary>
+        /// MojoTextArea12 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox MojoTextArea12;
+        
+        /// <summary>
+        /// MojoTextArea13 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox MojoTextArea13;
+        
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// divCleared control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::mojoPortal.Web.UI.EmptyPanel divCleared;
+        
+        /// <summary>
+        /// cbottom1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::mojoPortal.Web.Controls.CornerRounderBottom cbottom1;
     }
 }
